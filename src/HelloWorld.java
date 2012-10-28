c3426e1a460aa73ad6f34084e8550cde2c54a31f
scottsummers <summers_s@hotmail.com>
2012-10-27 21:50:00 -0400

    More Code
    Really I'm just trying to refresh my Java Brain. It's been 10 years ok.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,25 +10,49 @@
  static private PrintWriter screen = new PrintWriter (System.out, true);
  static private BufferedReader keyboard = new BufferedReader(new InputStreamReader (System.in));
 
- static private double degF;
- static private double degC;
  // classs methods
+ 
+ private static void helloWorld(){
+	 screen.println("Hello World!");
+ }
 
+ private static void degreeFtoDegreeC(){
+	 screen.print("\nEnter A degree in F >"); screen.flush();
+	 try {
+		 double degreeF = new Double (keyboard.readLine()).doubleValue();
+		 double degreeC = (((5.0/9.0)*degreeF)-(160.0/9.0));
+		 screen.println("\n"+ degreeF + " Degrees F equals " + degreeC + " Degrees C");
+	 } catch (Exception e) {
+		 screen.println("Sorry that was invalid.");
+	 }
+ }
+ 
+ private static void whatIsYourAge(){
+	 boolean exceptionloop = true;
+	 while(exceptionloop == true){
+		 try {
+			 screen.print("\nEnter your name > ");
+			 screen.flush();
+			 String name = new String (keyboard.readLine());
+			 screen.print("\nEnter your age > ");
+			 screen.flush();
+			 int age = new Integer (keyboard.readLine()) .intValue();
+			 screen.println("\n " + name + " is " + age + " years old.");
+			 exceptionloop = false;
+		 } catch (Exception e){
+			 screen.println("Sorry somewhere along the line a bad value go inputed try again.");
+			 exceptionloop = true;
+		 }
+	 }
+ }
+ 
+ 
  public static void main(String argv[]) throws IOException
   {
-    screen.print("\nEnter A degree in F >");
-    screen.flush();
-    degF = new Double (keyboard.readLine()).doubleValue();
-
-    //screen.print("\nEnter your age > ");
-    //screen.flush();
-    //age = new Integer (keyboard.readLine()) .intValue();
-
-     degC = (((5.0/9.0)*degF)-(160.0/9.0));
-      //return degC;
-
-    screen.println("\n"+ degF + "Degrees F is " + degC + " Degrees C");
-    //screen.println("\n " + name + " is " + age + " years old.");
+	helloWorld();
+    degreeFtoDegreeC();
+    whatIsYourAge();
+    
 
     screen.print("\nPress Enter to terminate.");
     screen.flush();
